92ef10257f29f8f50c970f7a6b4cab4c571c214b
shreyaschaudhari04 <shreyasrchaudhari@gmail.com>
2024-12-20 23:08:53 +0530

    Added Configuration for Lombok in POJO

--- src/main/java/com/shreyas/Journal/entity/JournalEntry.java
+++ src/main/java/com/shreyas/Journal/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.shreyas.Journal.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +10,7 @@ import java.time.LocalDateTime;
 
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -18,35 +22,5 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId(){
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setId(ObjectId id){
-        this.id = id;
-    }
 }
