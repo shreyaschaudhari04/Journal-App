fa7d9253aaf77848b56f04539f751a744cad1c90
shreyaschaudhari04 <shreyasrchaudhari@gmail.com>
2024-12-29 23:17:32 +0530

    Added Admin Role Authorization

---
+++ src/main/java/com/shreyas/Journal/controller/AdminController.java
@@ -0,0 +1,33 @@
+package com.shreyas.Journal.controller;
+
+
+import com.shreyas.Journal.entity.User;
+import com.shreyas.Journal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getALlUsers(){
+        List<User> all = userService.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-admin-user")
+    public void createUser(@RequestBody User user){
+        userService.saveAdmin(user);
+    }
+}
